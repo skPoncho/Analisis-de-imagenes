40fee7c1188afadaa25599abe4f5099269b95a50
poncho <carloslego.br@gmail.com>
2020-11-29 22:43:57 -0600

    actializacion de la ecualizacion

--- ecualizacion.py
+++ ecualizacion.py
@@ -13,35 +13,24 @@ def crear_imagen(tipo_imagen,tamanio):
     nueva_imagen = Image.new(tipo_imagen, tamanio)
     return nueva_imagen
 
-def guardar_imagen(imagen,datos,nombre_imagen):
-    imagen.putdata(datos)
-    imagen.save(nombre_imagen)
-    imagen.show()
-
 def ecualizacion(img):#obtiene la ecualizacion en escala de grises
-    imagen = abrir_imagen(img)#se abre la imagen inicial
+    imagen = Image.open(img).convert("L")
     histogramImagen  = imagen.histogram()
     datos = imagen.getdata()#se obtienen las matrices de la imagen
+    gmin = 0
+    gmax = 0
+    print("histograma imagen original ")
+    print(histogramImagen)
 
-
-    n_g = [256]
-    pg_g = [256]
+    pg_g = [0]
     ecualizacion = []
-    n_g = imagen.histogram()#SE SACAN
-
-    #for x in range(1,256):
-        #contador = 0
-        #for y in range(len(datos)):
-            #if(datos[y] == x):
-                #contador = contador + 1
-        #n_g.append(contador)
-
+    f_g = []
+    n_g = imagen.histogram()#SE SACAN los valores de gris
 
     cant_datos = len(datos)
+    p_g = [(n_g[x-1]/cant_datos) for x in range(1,257)]#se saca la probabilidad
 
-    p_g = [(n_g[x-1]/cant_datos) for x in range(1,256)]
-
-    for x in range(len(p_g)):
+    for x in range(len(p_g)):#probabilidad acumulada
         if(x==0):
             pg_g[0] = p_g[0]
         else:
@@ -50,18 +39,34 @@ def ecualizacion(img):#obtiene la ecualizacion en escala de grises
                 valor = 1
             pg_g.append(valor)
 
+    for x in range(len(n_g)):#se busca gmin
+        if(n_g[x] != 0):
+            gmin = x
+            break
+    for x in range(255,-1,-1):#se busca gmax
+        if(n_g[x] != 0):
+            gmax = x
+            break
+    resta_cubos = (gmax**(1/3))-(gmin**(1/3))
 
-    ecualizacion = [((6.34133*pg_g[x])**(1/3)) for x in range(len(pg_g))]
+    for x in range(0,256):
+        valor = (resta_cubos * pg_g[x]+(gmin**(1/3)))**3
+        f_g.append(valor)
 
+    ecualizacion = [(f_g[datos[x]]) for x in range(len(datos))]
 
-    imagen_ecualizacion = crear_imagen('L', imagen.size)
+    imagen_ecualizacion = Image.new('L', imagen.size)
+    #imagen_ecualizacion = crear_imagen('L', imagen.size)
 
-    guardar_imagen(imagen_ecualizacion,ecualizacion,"imagenes/ecualizacion_img.jpg")#se guardan los datos de la imagen nueva
+    #guardar_imagen(imagen_ecualizacion,ecualizacion,"imagenes/ecualizacion_img.jpg")#se guardan los datos de la imagen nueva
+    imagen_ecualizacion.putdata(ecualizacion)
+    imagen_ecualizacion.save("imagenes/ecualizacion_img.jpg")
+    imagen_ecualizacion.show()
 
     histogramImagen  = imagen_ecualizacion.histogram()
-    for x in range(len(histogramImagen)):
-        print("valor en  : "+str(x)+" es : "+str(histogramImagen[x]))
+    print("histograma imagen procesada ")
+    print(histogramImagen)
 
     cerrar_imagen(imagen_ecualizacion)#se cierra la imagen creada
     cerrar_imagen(imagen)
-ecualizacion("imagenes/foto_escala_grises.jpg")
+ecualizacion("imagenes/prueba.jpg")
